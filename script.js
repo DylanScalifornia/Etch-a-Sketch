31a84ba4c888cc8b9e051420b8cab3812a0e642c
DylanScalifornia <dylanscalifornia@gmail.com>
2024-05-29 11:50:07 -0700

    added random color for etch-sketch and changing color by opacity 10% of black

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const sizeOfGrid = 16;
 const resetButton = document.querySelector("button")
 const container = document.querySelector(".container")
 
+const createRandomRGB = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return {r, g, b}
+}
+
 const createGrid = (amtOfGrids) => {
   const wrapper = document.createElement("div")
   wrapper.classList.add("wrapper")
@@ -11,14 +19,21 @@ const createGrid = (amtOfGrids) => {
     row.classList.add("grid-row")
 
     for (let j = 0; j < amtOfGrids; j++){
-        const widthAndHeight =  960 / sizeOfGrid
+        const widthAndHeight =  960 / amtOfGrids
         const gridBox = document.createElement("div")
         gridBox.classList.add("grid-box")
         gridBox.style.width = `${widthAndHeight}px`
         gridBox.style.height = `${widthAndHeight}px`
         
         gridBox.addEventListener("mouseenter", () => {
-            gridBox.style.backgroundColor = "black"
+            const currentOpacity = gridBox.style.opacity
+            gridBox.style.background = `rgb(0, 0, 0)`
+            if (currentOpacity){
+             gridBox.style.opacity = Number(currentOpacity) + .1   
+            } else {
+                gridBox.style.opacity = .1
+            }
+            
         })
        
         row.appendChild(gridBox)
